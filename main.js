4a65a7aff836738b952096d5c9fe16c428a042b1
AslanSoft <beggyplayy@gmail.com>
2025-10-30 18:45:56 +0500

    new homework

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-const sign = document.querySelector('#signup-form')
-const input = document.querySelector('#username')
-const test = document.querySelector('#test')
 
-sign.addEventListener('submit', (e) => {  // ← событие ставим на форму!
-    e.preventDefault()
+const productList = document.querySelector('.products-list')
 
-    const regEx = /^[a-zA-Z0-9]{6,12}$/   // только латинские буквы и цифры, 6–12 символов
-    const inputVal = input.value           // ← берём значение из input
+fetch("https://fakestoreapi.com/products/")
+.then(response => response.json())
+.then(products => {
+    products.forEach((product) => {
+        const card = document.createElement('div');
+        card.classList.add('product');
 
-    if (regEx.test(inputVal)) {
-        test.textContent = "Qiymat to'g'ri ✅"
-        test.style.color = "lime"
-    } else {
-        test.textContent = "Qiymat noto'g'ri ❌"
-        test.style.color = "red"
-    }
+        const title = document.createElement('h2')
+        title.textContent = product.title;
+
+        const price = document.createElement('strong')
+        price.textContent = product.price
+
+        card.appendChild(title)
+        card.appendChild(price)
+
+        productList.appendChild(card)
+    })
 })
