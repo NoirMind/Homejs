50ab89985bced58e801b9cdaf61644c9591a4f1b
AslanSoft <beggyplayy@gmail.com>
2025-11-01 18:23:59 +0500

    fetch

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
+// fetch("https://fakestoreapi.com/products/")
+// .then(response => response.json())
+// .then(products => {
+//     products.forEach((product) => {
+//         const card = document.createElement('div');
+//         card.classList.add('product');
+
+//         const title = document.createElement('h2')
+//         title.textContent = product.title;
+
+//         const price = document.createElement('strong')
+//         price.textContent = product.price
+
+//         card.appendChild(title)
+//         card.appendChild(price)
+
+//         productList.appendChild(card)
+//     })
+// })
 
 const productList = document.querySelector('.products-list')
 
-fetch("https://fakestoreapi.com/products/")
-.then(response => response.json())
-.then(products => {
-    products.forEach((product) => {
-        const card = document.createElement('div');
-        card.classList.add('product');
+fetch('https://fakestoreapi.com/users')
+  .then(response => response.json())
+  .then(users => {
+    users.forEach((user) => {
+        const card = document.createElement('div')
+        card.classList.add('user')
 
-        const title = document.createElement('h2')
-        title.textContent = product.title;
+        const ism = document.createElement('h1')
+        ism.textContent = user.username
 
-        const price = document.createElement('strong')
-        price.textContent = product.price
+        const narx = document.createElement('strong')
+        narx.textContent = narx.email
 
-        card.appendChild(title)
-        card.appendChild(price)
+        card.appendChild(ism)
+        card.appendChild(narx)
 
         productList.appendChild(card)
     })
-})
+  });
+
+
+  
