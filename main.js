a3400a931a42747acbcaa14dd5de6671b6560285
AslanSoft <beggyplayy@gmail.com>
2025-11-03 20:07:31 +0500

    this

--- main.js
+++ main.js
@@ -18,51 +18,51 @@
 //     })
 // })
 
-const productList = document.querySelector('.products-list')
+// const productList = document.querySelector('.products-list')
 
-fetch('https://fakestoreapi.com/users')
-  .then(response => response.json())
-  .then(users => {
-    users.forEach((user) => {
-        const card = document.createElement('div')
-        card.classList.add('user')
+// fetch('https://fakestoreapi.com/users')
+//   .then(response => response.json())
+//   .then(users => {
+//     users.forEach((user) => {
+//         const card = document.createElement('div')
+//         card.classList.add('user')
 
-        const ism = document.createElement('h1')
-        ism.textContent = user.username
+//         const ism = document.createElement('h1')
+//         ism.textContent = user.username
 
-        const narx = document.createElement('strong')
-        narx.textContent = narx.email
+//         const narx = document.createElement('strong')
+//         narx.textContent = narx.email
 
-        card.appendChild(ism)
-        card.appendChild(narx)
+//         card.appendChild(ism)
+//         card.appendChild(narx)
 
-        productList.appendChild(card)
-    })
-  });
+//         productList.appendChild(card)
+//     })
+//   });
 
-  class BankAccount{
-    #balance;
+//   class BankAccount{
+//     #balance;
 
-    constructor (owner, balance) {
-      this.owner = owner
-      this.balance = balance
-    }
+//     constructor (owner, balance) {
+//       this.owner = owner
+//       this.balance = balance
+//     }
 
-    pulTushurish(amount){
-      this.#balance += amount;
-    }
+//     pulTushurish(amount){
+//       this.#balance += amount;
+//     }
 
-    pulMiqdori() {
-      return `${this.owner}'s balanse is $${this.balance}`;
-    }
+//     pulMiqdori() {
+//       return `${this.owner}'s balanse is $${this.balance}`;
+//     }
 
 
     
-  }
+//   }
 
-      const acc = new BankAccount ("IdenityNull", 1000);
-    acc.pulTushurish(500);
-    console.log(acc.pulMiqdori());
+//       const acc = new BankAccount ("IdenityNull", 1000);
+//     acc.pulTushurish(500);
+//     console.log(acc.pulMiqdori());
 
 // encaptulation usuli bu 
 
@@ -88,42 +88,68 @@ fetch('https://fakestoreapi.com/users')
 
 // abstraction
 
-class hayvon {
-  constructor (name){
-    this.name = name
-  }
+// class hayvon {
+//   constructor (name){
+//     this.name = name
+//   }
 
-  ovoz() {
-    console.log(`${this.name} ovoz chiqaryabdi`);
-  }
+//   ovoz() {
+//     console.log(`${this.name} ovoz chiqaryabdi`);
+//   }
+// }
+
+//   class Mushuk extends hayvon {
+//     ovoz () {
+//       console.log(`${this.name} mewolaydi`); 
+//     }
+//   }
+
+//   const mushuk = new Mushuk("Mushuk")
+
+//   mushuk.ovoz()
+
+//   //inheritance 
+
+//   class it extends hayvon {
+//     ovoz (){
+//       console.log("Bu it");
+      
+//     }
+//   }
+
+//   class mol extends hayvon {
+//     ovoz(){
+//       console.log("bu mol");
+      
+//     }
+//   }
+
+//   const hayvonlar = [new it(), new mol()]
+//   hayvonlar.forEach(hayvon => hayvon.ovoz())
+//   console.log(hayvonlar);
+
+class Faris {
+    constructor(name, age, hobby){
+      this.name = name;
+      this.age = age;
+      this.hobby = hobby
+    }
+
+    ismi (){
+      console.log(`${this.name} 😼 uxlayabdi`);
+    }
+
+    yoshi (){
+      console.log(`😼ning yoshi ${this.age}`);
+    }
+
+    hobbysi(){
+      console.log(`😼ning hobbysi bu ${this.hobby}`);
+    }
 }
 
-  class Mushuk extends hayvon {
-    ovoz () {
-      console.log(`${this.name} mewolaydi`); 
-    }
-  }
-
-  const mushuk = new Mushuk("Mushuk")
-
-  mushuk.ovoz()
-
-  //inheritance 
-
-  class it extends hayvon {
-    ovoz (){
-      console.log("Bu it");
-      
-    }
-  }
-
-  class mol extends hayvon {
-    ovoz(){
-      console.log("bu mol");
-      
-    }
-  }
-
-  const hayvonlar = [new it(), new mol()]
-  hayvonlar.forEach(hayvon => hayvon.ovoz())
-  console.log(hayvonlar);
+const faris = new Faris("Faris", 1, "uxlash");
+console.log(faris);
+faris.ismi();
+faris.yoshi();
+faris.hobbysi();
